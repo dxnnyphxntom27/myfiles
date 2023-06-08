438a0e76bee1996de6212cace23cb4d80da43b5f
Daniel Dybowski <93659264+dxnnyphxntom27@users.noreply.github.com>
2023-06-08 14:15:26 +0200

    Add files via upload

---
+++ cplabmi/cplabmi/cplabmi.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Area
+{
+public:
+	int areav = 0;
+	int myArea(int len, int wid)
+	{
+		areav = 2 * (len + wid);
+		return areav;
+	}
+	~Area()
+	{
+		cout << "Area has been destroyed." << endl;
+	}
+};
+
+class Perimeter
+{
+public:
+	int perimv = 0;
+	int myPerimeter(int len, int wid)
+	{
+		perimv = 2 * (len + wid);
+		return perimv;
+	}
+	~Perimeter()
+	{
+		cout << "Rectangle has been destroyed." << endl;
+	}
+
+};
+
+class Rectangle : public Area, public Perimeter
+{
+	int reclen;
+	int recwid;
+public:
+	Rectangle()
+	{
+		reclen = 2;
+		recwid = 3;
+	}
+	int area()
+	{
+		return myArea(reclen, recwid);
+	}
+	int perimeter()
+	{
+		return myPerimeter(reclen, recwid);
+	}
+	~Rectangle()
+	{
+		cout << "Rectangle has been destroyed." << endl;
+	}
+};
